7bd76e56fd8f3ab99763a34bf98ba3ef84ddd2be
cuongtm191221 <cuongtm191221@gmail.com>
2024-05-16 21:09:46 +0700

    Upload bài tập của session 04

---
+++ index.js
@@ -0,0 +1,28 @@
+// Boolean
+// let isLoggedIn = true;
+
+// let gameOver = false;
+
+// let isWaterWet = true;
+
+// console.log(isLoggedIn);
+
+// console.log(1 > 1); // false
+
+// console.log(1 >= 1); // true
+
+// console.log(1 < 1); // false
+
+// console.log(1 <= 1); // false
+
+// console.log(1 == 1); //  true
+
+// console.log(1 == "1"); // true
+
+// console.log(1 != "1"); //  false
+
+// console.log(1 === "1"); //  false
+
+// console.log(1 !== "1"); //  true
+
+//
